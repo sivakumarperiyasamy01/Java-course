8abd6ad670522f154905043959c605e5ca1d5075
sivakumarperiyasamy01 <139151119+sivakumarperiyasamy01@users.noreply.github.com>
2025-04-09 23:26:10 +0530

    Interface Excercises

---
+++ Operations.java
@@ -0,0 +1,32 @@
+interface Operation {
+    int perform(int x, int y);
+}
+
+// Complete the classes below by implementing the Operation interface and providing your own implementation of the perform method.
+class Add implements Operation {
+    @Override
+    public int perform(int x, int y) {
+       return x+y;
+    }
+}
+
+class Subtract implements Operation {
+    @Override
+    public int perform(int x, int y) {
+      return x-y;
+    }
+}
+
+class Divide implements Operation {
+    @Override
+    public int perform(int x, int y) {
+    if(y==0){
+        return -1;
+    }
+    
+    return x/y;
+     
+     
+    }
+      
+}
